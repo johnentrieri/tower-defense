efaa06e372589a76c5aaef59e2f5e51330cbed13
John Entrieri <john.entrieri@gmail.com>
2020-12-10 12:15:17 -0500

    Added Code Enforced Grid Snapping

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] float gridSize = 2.0f;
+    private Vector3 snapPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        snapPosition.x = Mathf.Round(transform.position.x / gridSize) * gridSize;
+        snapPosition.y = 0;
+        snapPosition.z = Mathf.Round(transform.position.z / gridSize) * gridSize;
+        
+        transform.position = snapPosition;
+    }
+}
