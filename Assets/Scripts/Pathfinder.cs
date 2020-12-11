d680e7a240b0777e3328ba52604a2fc444438c36
John Entrieri <john.entrieri@gmail.com>
2020-12-10 20:08:20 -0500

    Added Explore Neighbors Method

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -8,17 +8,32 @@ public class Pathfinder : MonoBehaviour
     [SerializeField] Block endBlock;
     private Dictionary<Vector2Int,Block> grid = new Dictionary<Vector2Int, Block>();
 
+    private Vector2Int[] allowedDirections = {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
-        SetStartAndEndColors();        
+        SetStartAndEndColors();
+        ExploreNeighbors(startBlock);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void ExploreNeighbors(Block block) {
+        foreach (Vector2Int direction in allowedDirections) {
+            Vector2Int explorePos = block.GetGridPos() + direction;
+            if (grid.ContainsKey(explorePos)) { grid[explorePos].SetTopColor(Color.yellow); }
+        }
     }
 
     private void SetStartAndEndColors() {
