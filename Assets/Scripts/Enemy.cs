a286d16ee9eb8f6a33d5b6f183762982cb80eb4b
DESKTOP-JOHN\johne <john.entrieri@gmail.com>
2021-02-06 21:13:23 -0500

    Added Bullet Hit Effect for Enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject explosion;
+    [SerializeField] ParticleSystem hitEffect;
+
     private int enemyHealth = 1;
     private Transform explosionSpawnParent;
 
     void OnParticleCollision(GameObject other) {
 
+        hitEffect.Play();
         if ( --enemyHealth <= 0) {
             ProcessEnemyDeath();
         }
