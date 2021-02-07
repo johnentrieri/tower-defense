dbb983cf6cf7c4e9dc550e2cb99dbb08d4885900
DESKTOP-JOHN\johne <john.entrieri@gmail.com>
2021-02-07 13:15:53 -0500

    Created TowerFactory Class

--- Assets/Scripts/TowerBlock.cs
+++ Assets/Scripts/TowerBlock.cs
@@ -4,69 +4,47 @@ using UnityEngine;
 
 public class TowerBlock : MonoBehaviour
 {
-    [SerializeField] Tower spawnTower;
-    [SerializeField] Transform spawnParent;
-    private Color previewColor;
     private Color startingColor;
+    private TowerFactory towerFactory;
     private int gridSize;
-    private Tower tower;
+    private bool isTowerPlaced;
 
     void Start() {
         GetGridSize();
         startingColor = GetTopColor();
+        towerFactory = FindObjectOfType<TowerFactory>();
+        isTowerPlaced = false;
     }
 
     void OnMouseOver() {
-        SetPreviewColor( spawnTower.GetColor() );
-        SetTopColor(previewColor);        
+        SetTopColor(towerFactory.GetTowerColor());     
     }
 
     void OnMouseUpAsButton() {
-        if (tower != null) {
-            Destroy(tower.gameObject);
-        } else {
-            Vector2Int spawnPosition = GetGridPos();
-            tower = SpawnTower(spawnPosition);
-        }     
+        Vector2Int spawnPosition = GetGridPos();
+        if (!isTowerPlaced) { 
+            towerFactory.SpawnTower(this);
+            isTowerPlaced = true;
+        }
     }
 
     void OnMouseExit() {
         SetTopColor(startingColor);        
     }
 
-    
-    public void SetPreviewColor(Color color) {
-        print(color.ToString());
-        previewColor = color;
-    }
-
-    public void SetSpawnTower(Tower tower) {
-        print("Spawn");
-    }
-
-    private Tower SpawnTower(Vector2Int spawnPos) {
-        Vector3 spawnPosition = new Vector3( 
-            spawnPos.x * gridSize,
-            0,
-            spawnPos.y * gridSize
-        );
-        Tower spawnedTower = GameObject.Instantiate(spawnTower,spawnPosition,Quaternion.identity,spawnParent);
-        return spawnedTower;
-
-    }
-
-    private void GetGridSize() {
-        EnemyBlock tempBlock = gameObject.AddComponent<EnemyBlock>();
-        gridSize = tempBlock.GetGridSize();
-        Destroy(tempBlock);
-    }
-
-    private Vector2Int GetGridPos() {
+    public Vector2Int GetGridPos() {
         return new Vector2Int(
             Mathf.RoundToInt(transform.position.x / gridSize),
             Mathf.RoundToInt(transform.position.z / gridSize)
         );
     }
+
+    public int GetGridSize() {
+        EnemyBlock tempBlock = gameObject.AddComponent<EnemyBlock>();
+        gridSize = tempBlock.GetGridSize();
+        Destroy(tempBlock);
+        return gridSize;        
+    }    
 
     private void SetTopColor(Color color) {
         MeshRenderer topMeshRenderer = transform.Find("Top").GetComponent<MeshRenderer>();
---
+++ Assets/Scripts/TowerFactory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFactory : MonoBehaviour
+{
+    [SerializeField] Tower spawnTower;
+    [SerializeField] Transform spawnParent;
+    [SerializeField] int maxTowers;
+    private int towersPlaced;
+
+    void Start() {
+        towersPlaced = 0;
+    }
+
+    public Color GetTowerColor() {
+        return spawnTower.GetColor();
+    }
+
+    public void SpawnTower(TowerBlock spawnBlock) {
+        if (towersPlaced >= maxTowers) { return; }
+
+        Vector3 spawnPosition = new Vector3( 
+            spawnBlock.GetGridPos().x * spawnBlock.GetGridSize(),
+            0,
+            spawnBlock.GetGridPos().y * spawnBlock.GetGridSize()
+        );
+
+        GameObject.Instantiate(spawnTower,spawnPosition,Quaternion.identity,spawnParent);
+        towersPlaced++;
+    }
+
+    //TODO - Destroy Tower
+}
